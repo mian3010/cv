8398049bed97e1a91426347df0fac8d36bf147e6
Michael Søby Andersen <michael@reload.dk>
2018-12-06 15:46:24 +0100

    Add empty header, and remove bootstrap

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+/**
+ * Header component.
+ *
+ * @extends React.Component
+ */
+class Header extends Component {
+  /** @inheritdoc */
+  render() {
+    return (
+      <header>
+        Header here
+      </header>
+    );
+  }
+}
+
+export default Header;
